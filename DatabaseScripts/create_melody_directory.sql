a67063b2981844a48e7eea6d61da0c82a0278f2d
ShaneSchroeder25 <91928901+ShaneSchroeder25@users.noreply.github.com>
2021-10-15 12:54:35 -0400

    Add files via upload

---
+++ DatabaseScripts/create_melody_directory.sql
@@ -0,0 +1,26 @@
+﻿USE master
+GO
+
+/****** Object:  Database AP     ******/
+IF DB_ID('MelodyDirectory') IS NOT NULL
+	DROP DATABASE MelodyDirectory
+GO
+
+CREATE DATABASE MelodyDirectory
+GO 
+
+USE MelodyDirectory
+GO
+
+/****** Object:  Table Profiles  ******/   
+CREATE TABLE Profiles(
+	userId		VARCHAR(20)			NOT NULL,
+	pass		VARCHAR(20)			NOT NULL
+)
+GO
+
+/****** Object:  Table ObjectInfo  ******/   
+CREATE TABLE ObjectInfo(
+	info		VARCHAR(1024)		NOT NULL
+)
+GO
