fbff6b3a8b1788371da7881328d1b76bec8a7419
Bricen Raynold <91922446+Raynolbj@users.noreply.github.com>
2021-10-20 00:22:32 -0400

    Add files via upload

---
+++ MelodyDirectory/src/main/resources/com/levi/melodydirectory/Artist.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Artist extends Generic<Artist> {
+	/*
+	 * Artist Class
+	 * (Everything in the Generic) with;
+	 * ArrayList of albums
+	 * toString()
+	 * tags() (Gets the genre)
+	 */
+	
+	private ArrayList<Album> albums = new ArrayList<Album>();
+	
+	public Artist(String name, String description, String genre, String releaseDate, String eLink, ArrayList<Album> albums) {
+		super(name, description, genre, releaseDate, eLink);
+		setDataType(3);
+		this.albums = albums;
+	}
+	
+	// dataType, name, description, genre, releaseDate, eLink, albums ArrayList
+	public String toString() {
+		return super.toString() + "," + albums;
+	}
+	
+	public String tags() {
+		return super.tags();
+	}
+
+	public ArrayList<Album> getAlbums() {
+		return albums;
+	}
+
+	public void setAlbums(ArrayList<Album> albums) {
+		this.albums = albums;
+	}
+	
+}
