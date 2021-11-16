a314af0078a4a0941089c61a32637ef2710cb58d
Isaiah Kohl <92473681+Kohlil@users.noreply.github.com>
2021-11-16 13:01:36 -0500

    Add files via upload

--- MelodyDirectory/src/test/java/com/levi/melodydirectory/ArtistTest.java
+++ MelodyDirectory/src/test/java/com/levi/melodydirectory/ArtistTest.java
@@ -1,3 +1,5 @@
+package com.levi.melodydirectory;
+
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -7,12 +9,12 @@ class ArtistTest {
 
 	@Test
 	void testToString() {
-		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A", "Marvel Studios", "SpiderMan Soundtrack", "$2.00", "2:00");
-		ArrayList<Song> songs = new ArrayList<Song>();
-		songs.add(SF);
+		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A","SpiderMan Soundtrack", "$2.00", "2:00", "Post Malone");
+		ArrayList<String> songs = new ArrayList<>();
+		songs.add("Sunflower");
 		Album spiderMan = new Album("SpiderMan Into the SpiderVerse", "Movie Soundtrack", "Pop", "10/18/18", "N/A", "Various Artists", songs, "Marvel Studios", "$2.00");
-		ArrayList<Album> albums = new ArrayList<Album>();
-		albums.add(spiderMan);
+		ArrayList<String> albums = new ArrayList<>();
+		albums.add("SpiderMan Into the SpiderVerse");
 		Artist marvel = new Artist("Marvel", "Soundtrack for SpiderMan ITSV", "Pop", "Marvel.com", albums);
 		assertEquals("3,Marvel,Soundtrack for SpiderMan ITSV,Pop,10/18/18,Marvel.com,[2,SpiderMan Into the SpiderVerse,Movie Soundtrack,10/18/18,N/A,Pop,Various Artists,[1,Sunflower,From SpiderMan, Into The Spiderverse,Pop,10/18/18,N/A,SpiderMan Soundtrack,$2.00,2:00],Marvel Studios,$2.00]" , marvel.toString());
 		assertEquals("Wrong".equals(marvel.toString()), false);
@@ -20,12 +22,12 @@ class ArtistTest {
 
 	@Test
 	void testArtist() {
-		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A", "Marvel Studios", "SpiderMan Soundtrack", "$2.00", "2:00");
-		ArrayList<Song> songs = new ArrayList<Song>();
-		songs.add(SF);
+		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A", "SpiderMan Soundtrack", "$2.00", "2:00", "Post Malone");
+		ArrayList<String> songs = new ArrayList<>();
+		songs.add("Sunflower");
 		Album spiderMan = new Album("SpiderMan Into the SpiderVerse", "Movie Soundtrack", "Pop", "10/18/18", "N/A", "Various Artists", songs, "Marvel Studios", "$2.00");
-		ArrayList<Album> albums = new ArrayList<Album>();
-		albums.add(spiderMan);
+		ArrayList<String> albums = new ArrayList<>();
+		albums.add("SpiderMan Into the SpiderVerse");
 		Artist marvel = new Artist("Marvel", "Soundtrack for SpiderMan ITSV", "Pop", "Marvel.com", albums);
 		assertEquals("Marvel", marvel.getName());
 		assertEquals("Soundtrack for SpiderMan ITSV", marvel.getDescription());
@@ -37,16 +39,16 @@ class ArtistTest {
 
 	@Test
 	void testGetAlbums() {
-		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A", "Marvel Studios", "SpiderMan Soundtrack", "$2.00", "2:00");
-		Song SF2 = new Song("Sunflower", "From SpiderMan", "Pop", "10/18/18", "N/A", "Marvel Studios", "SpiderMan Soundtrack", "$2.00", "2:00");
-		ArrayList<Song> songs = new ArrayList<Song>();
-		ArrayList<Song> songs2 = new ArrayList<Song>();
-		songs.add(SF);
-		songs2.add(SF2);
+		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A", "SpiderMan Soundtrack", "$2.00", "2:00", "Post Malone");
+		Song SF2 = new Song("Sunflower", "From SpiderMan, ITSV", "Pop", "10/18/18", "N/A", "SpiderMan Soundtrack", "$2.00", "2:00", "Post Malone");
+		ArrayList<String> songs = new ArrayList<>();
+		ArrayList<String> songs2 = new ArrayList<>();
+		songs.add("Sunflower");
+		songs2.add("Sunflower");
 		Album spiderMan = new Album("SpiderMan ITSV", "Movie Soundtrack", "Pop", "10/18/18", "N/A", "Various Artists", songs, "Marvel Studios", "$2.00");
-		ArrayList<Album> albums = new ArrayList<Album>();
-		ArrayList<Album> albums2 = new ArrayList<Album>();
-		albums.add(spiderMan);
+		ArrayList<String> albums = new ArrayList<>();
+		ArrayList<String> albums2 = new ArrayList<>();
+		albums.add("SpiderMan Into the SpiderVerse");
 		Artist marvel = new Artist("Marvel", "Soundtrack for SpiderMan ITSV", "Pop", "Marvel.com", albums);
 		assertEquals(albums, marvel.getAlbums());
 		marvel.setAlbums(albums2);
@@ -55,16 +57,16 @@ class ArtistTest {
 
 	@Test
 	void testSetAlbums() {
-		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A", "Marvel Studios", "SpiderMan Soundtrack", "$2.00", "2:00");
-		Song SF2 = new Song("Sunflower", "From SpiderMan", "Pop", "10/18/18", "N/A", "Marvel Studios", "SpiderMan Soundtrack", "$2.00", "2:00");
-		ArrayList<Song> songs = new ArrayList<Song>();
-		ArrayList<Song> songs2 = new ArrayList<Song>();
-		songs.add(SF);
-		songs2.add(SF2);
+		Song SF = new Song("Sunflower", "From SpiderMan, Into The Spiderverse", "Pop", "10/18/18", "N/A", "SpiderMan Soundtrack", "$2.00", "2:00", "Post Malone");
+		Song SF2 = new Song("Sunflower", "From SpiderMan, ITSV", "Pop", "10/18/18", "N/A", "SpiderMan Soundtrack", "$2.00", "2:00", "Post Malone");
+		ArrayList<String> songs = new ArrayList<>();
+		ArrayList<String> songs2 = new ArrayList<>();
+		songs.add("Sunflower");
+		songs2.add("Sunflower");
 		Album spiderMan = new Album("SpiderMan ITSV", "Movie Soundtrack", "Pop", "10/18/18", "N/A", "Various Artists", songs, "Marvel Studios", "$2.00");
-		ArrayList<Album> albums = new ArrayList<Album>();
-		ArrayList<Album> albums2 = new ArrayList<Album>();
-		albums.add(spiderMan);
+		ArrayList<String> albums = new ArrayList<>();
+		ArrayList<String> albums2 = new ArrayList<>();
+		albums.add("SpiderMan Into the SpiderVerse");
 		Artist marvel = new Artist("Marvel", "Soundtrack for SpiderMan ITSV", "Pop", "Marvel.com", albums);
 		assertEquals(albums, marvel.getAlbums());
 		marvel.setAlbums(albums2);
